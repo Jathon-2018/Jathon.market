0910ce69cefd1c10aa81fad0fab596a17a42cd42
Jathon <jathon.market2018@gmail.com>
2018-11-23 12:07:39 +0700

    Update appFuctions.js

--- appFuctions.js
+++ appFuctions.js
@@ -431,7 +431,7 @@ exports.shipping = function (req, res)
             if (err) throw err;
             //เลือก ใบสั่งอาหารจาก user_id & store_id ที่เวลาล่า
 
-            sql = `SELECT * FROM shipping WHERE shipping_date_check = (SELECT MAX(shipping_date) FROM shipping WHERE shipping_store_id = ? and shipping_users_id = ?) `;
+            sql = `SELECT * FROM shipping WHERE shipping_date_check = (SELECT MAX(shipping_date_check) FROM shipping WHERE shipping_store_id = ? and shipping_users_id = ?) `;
             con.query(sql, [shipping_store_id,shipping_users_id], function (err, result){
                 if (err) throw err;
                 var Neworders_id = result[0].shipping_id 
