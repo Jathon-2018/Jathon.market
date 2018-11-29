9800241d044eb975d647b42ce7101e813e33c81f
Jathon <jathon.market2018@gmail.com>
2018-11-30 00:36:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -107,6 +107,7 @@ app.get('/', function (req, res) {
 
   //---- updateToken --------------------
   app.post('/updateToken',function(req,res){
+    console.log(req.body.token)
     appFuctions.updateToken(req,res);
   });
 
