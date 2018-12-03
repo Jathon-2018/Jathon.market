9ec2ed1f433554d01dd5d15bd2b3e4615a21be6b
Jathon <jathon.market2018@gmail.com>
2018-12-03 17:52:10 +0700

    Update fcm.js

--- routes/fcm.js
+++ routes/fcm.js
@@ -28,7 +28,7 @@ router.post('/sendMessage', function (req, res, next) {
       let sender = new gcm.Sender('AAAA4gLgBcE:APA91bGWvIzWvKWgpW86YcG4UK7BNGO-qk-33Zi2VZcNlK9H1hrjY5YUTkVbKQEKTPfzz6lBJ_u3pt1UAJmCzhWUjfj6qo8JSl8XRKHn4C_pimUyZ1oxbsIiJMSyfJjWvZcVhS_cqsD6');
 
       var regTokens = ["dafjtZM4udM:APA91bFKhvc17Fvyc0bXpGd9eBjTZ6AErPnA5z1NtHvWFWOVO0-UhEC__fd_xoe9nidCXZvigbA8gUYaY6wVSo6M_F_vAeHheHwz5peDhLm3SlgbU_F9fyaEDU4hAQEak8wiqDOoqZLQ"];
-      sender.send(message, { registrationTokens: regTokens }, (err, response) => {
+      sender.send(message, { registrationTokens: tokens }, (err, response) => {
         if (err) {
           console.log(err);
           res.send({ ok: false, error: err });
