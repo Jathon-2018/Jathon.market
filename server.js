36cd47944c57f10bc1efbf88f6f31e46d4af155e
Jathon <jathon.market2018@gmail.com>
2018-12-03 18:38:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -50,44 +50,44 @@ app.get('/', function (req, res) {
   console.log("homeStart")
 })
 
-app.post('/sendMessage', function (req, res) {
-  var gcm = require('node-gcm');
+// app.post('/sendMessage', function (req, res) {
+//   var gcm = require('node-gcm');
 
-  let store_name = req.messageData.store_name;
-  let statusSend = req.messageData.statusSend;
-  let datatoken = req.messageData.datatoken;
+//   let store_name = req.messageData.store_name;
+//   let statusSend = req.messageData.statusSend;
+//   let datatoken = req.messageData.datatoken;
 
-  console.log(store_name, statusSend,datatoken);
+//   console.log(store_name, statusSend,datatoken);
   
-      let tokens = [];
-      for(var i = 0; i < datatoken.length; i++){
-        tokens.push(rows[i].datatoken);
-      }
-      console.log('dataToken',tokens);
-      let message = new gcm.Message();
-      message.addData('title', 'ร้าน', store_name);
-      message.addData('message', statusSend);
-      message.addData('content-available', true);
-    //   message.addData('data', { "username": "Satit", "message": "Hello world" });
-      message.addData('image', 'http://www.pro.moph.go.th/w54/images/ICT/loadlogomoph.png');
+//       let tokens = [];
+//       for(var i = 0; i < datatoken.length; i++){
+//         tokens.push(rows[i].datatoken);
+//       }
+//       console.log('dataToken',tokens);
+//       let message = new gcm.Message();
+//       message.addData('title', 'ร้าน', store_name);
+//       message.addData('message', statusSend);
+//       message.addData('content-available', true);
+//     //   message.addData('data', { "username": "Satit", "message": "Hello world" });
+//       message.addData('image', 'http://www.pro.moph.go.th/w54/images/ICT/loadlogomoph.png');
 
-      let sender = new gcm.Sender('AAAA4gLgBcE:APA91bGWvIzWvKWgpW86YcG4UK7BNGO-qk-33Zi2VZcNlK9H1hrjY5YUTkVbKQEKTPfzz6lBJ_u3pt1UAJmCzhWUjfj6qo8JSl8XRKHn4C_pimUyZ1oxbsIiJMSyfJjWvZcVhS_cqsD6');
+//       let sender = new gcm.Sender('AAAA4gLgBcE:APA91bGWvIzWvKWgpW86YcG4UK7BNGO-qk-33Zi2VZcNlK9H1hrjY5YUTkVbKQEKTPfzz6lBJ_u3pt1UAJmCzhWUjfj6qo8JSl8XRKHn4C_pimUyZ1oxbsIiJMSyfJjWvZcVhS_cqsD6');
 
-      var regTokens = ["dafjtZM4udM:APA91bFKhvc17Fvyc0bXpGd9eBjTZ6AErPnA5z1NtHvWFWOVO0-UhEC__fd_xoe9nidCXZvigbA8gUYaY6wVSo6M_F_vAeHheHwz5peDhLm3SlgbU_F9fyaEDU4hAQEak8wiqDOoqZLQ"];
-      sender.send(message, { registrationTokens: tokens }, (err, response) => {
-        if (err) {
-          console.log(err);
-          res.send({ ok: false, error: err });
-        } else {
-          console.log(response);
-          res.send({ ok: true });
-        }
-      });
+//       var regTokens = ["dafjtZM4udM:APA91bFKhvc17Fvyc0bXpGd9eBjTZ6AErPnA5z1NtHvWFWOVO0-UhEC__fd_xoe9nidCXZvigbA8gUYaY6wVSo6M_F_vAeHheHwz5peDhLm3SlgbU_F9fyaEDU4hAQEak8wiqDOoqZLQ"];
+//       sender.send(message, { registrationTokens: tokens }, (err, response) => {
+//         if (err) {
+//           console.log(err);
+//           res.send({ ok: false, error: err });
+//         } else {
+//           console.log(response);
+//           res.send({ ok: true });
+//         }
+//       });
 
 
 
-  res.send('Token True')
-})
+//   res.send('Token True')
+// })
 
 
 
