d266f1c56c8cd17498a932fd6f2cf715f8f2cbd4
Jathon <jathon.market2018@gmail.com>
2018-12-03 18:15:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -181,4 +181,21 @@ app.get('/getAdmin',function(req,res){
   //   console.log('Runing 4000!')
   // })
 
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
 module.exports = app;
