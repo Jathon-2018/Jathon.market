289c0848e30c4969f71d6d9c4a06aac85f514840
Jathon <jathon.market2018@gmail.com>
2018-12-03 15:08:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ var fcm = require('./routes/fcm');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
